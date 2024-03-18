59c7e8f1bce3b82f1bbc3cc819c2750de38a4aff
pawelek <k.pawelek@vp.pl>
2024-03-18 22:16:35 +0100

    add btns

--- src/index.js
+++ src/index.js
@@ -8,23 +8,15 @@
 // //   });
 // // });
 
-// const hamburgerBtn = document.querySelector('.hamburger-btn');
-// const navbarList = document.querySelector('.navbar-list');
+const hamburgerBtn = document.querySelector('.hamburger-input');
+hamburgerBtn.checked = false;
+const navbarList = document.querySelector('.navbar-header');
+const header = document.querySelector('.header');
 
-// hamburgerBtn.addEventListener('click', e => {
-//   e.preventDefault();
-//   navbarList.classList.toggle('show');
-//   if (navbarList.classList.contains('show')) {
-//     const listItems = navbarList.querySelectorAll('li');
-//     let delay = 0;
-//     listItems.forEach((item, index) => {
-//       item.style.transitionDelay = `${delay}s`;
-//       item.classList.toggle('show');
-//       delay += 0.1;
-//     });
-//   } else {
-//     navbarList.querySelectorAll('li').forEach(item => {
-//       item.classList.remove('show');
-//     });
-//   }
-// });
+hamburgerBtn.addEventListener('click', e => {
+  navbarList.classList.add('active');
+
+  if (hamburgerBtn.checked == false) {
+    navbarList.classList.remove('active');
+  }
+});
