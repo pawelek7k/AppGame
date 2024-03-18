553763eb9c0d7dfedecde7e869f82af1bed2727d
pawelek7k <144838381+pawelek7k@users.noreply.github.com>
2024-03-18 21:16:56 +0000

    Deploying to gh-pages from @ pawelek7k/AppGame@59c7e8f1bce3b82f1bbc3cc819c2750de38a4aff 🚀

---
+++ index.130171de.js
@@ -0,0 +1,2 @@
+var hamburgerBtn=document.querySelector(".hamburger-input");hamburgerBtn.checked=!1;var navbarList=document.querySelector(".navbar-header"),header=document.querySelector(".header");hamburgerBtn.addEventListener("click",(function(e){navbarList.classList.add("active"),0==hamburgerBtn.checked&&navbarList.classList.remove("active")}));
+//# sourceMappingURL=index.130171de.js.map
