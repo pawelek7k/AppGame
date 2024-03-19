3bee86d6528dfc28a0a75d0e559ebd9cf3534055
pawelek <k.pawelek@vp.pl>
2024-03-19 15:06:25 +0100

    add navbar header

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@
 
 const hamburgerBtn = document.querySelector('.hamburger-input');
 hamburgerBtn.checked = false;
-const navbarList = document.querySelector('.navbar-header');
+const navbarList = document.querySelector('.navbar-list');
 const header = document.querySelector('.header');
 
 hamburgerBtn.addEventListener('click', e => {
-  navbarList.classList.add('active');
+  navbarList.style.opacity = 1;
 
   if (hamburgerBtn.checked == false) {
-    navbarList.classList.remove('active');
+    navbarList.style.opacity = 0;
   }
 });
