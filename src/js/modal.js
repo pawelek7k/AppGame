11834f8f5aba1839c60e3475179002ef0a95c63c
pawelek <k.pawelek@vp.pl>
2024-03-29 13:26:54 +0100

    add close btn modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,5 +5,13 @@ buttonsDetails.forEach(button => {
   button.addEventListener('click', e => {
     e.preventDefault();
     modalDetails.style.display = 'block';
+    console.log('dupa');
   });
+});
+
+const closeButton = modalDetails.querySelector('.close');
+modalDetails.addEventListener('click', e => {
+  if (e.target === modalDetails || e.target === closeButton) {
+    modalDetails.style.display = 'none';
+  }
 });
