77a69e3e3cfb93a03777b959c933126ba4bc9081
pawelek <k.pawelek@vp.pl>
2024-03-31 15:08:40 +0200

    add btns

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -5,11 +5,28 @@ let page = 1;
 
 let perPage = 40;
 
-loadMoreBtn.addEventListener('click', async () => {
+const prevPageBtn = document.querySelector('.prev-page-button');
+const nextPageBtn = document.querySelector('.next-page-button');
+
+prevPageBtn.addEventListener('click', async () => {
+  if (page > 1) {
+    try {
+      const data = await fetchPopularGames(page - 1);
+      renderGames(data.results);
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    } catch (error) {
+      console.error('Error fetching and rendering games:', error);
+    }
+  }
+});
+
+nextPageBtn.addEventListener('click', async () => {
   try {
-    const cards = await fetchPopularGames(page + 1);
-    renderGames(cards.results);
+    const data = await fetchPopularGames(page + 1);
+    renderGames(data.results);
     page++;
+
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   } catch (error) {
     console.error('Error fetching and rendering games:', error);
   }
