3d364b92e2e9256f5a0fb105c24cea44b6b0aa87
pawelek <k.pawelek@vp.pl>
2024-03-31 12:46:51 +0200

    add load more

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,1 +1,14 @@
+import { fetchPopularGames, page, renderGames } from './render-cards';
+const loadMoreBtn = document.querySelector('.load-more-button');
 
+let perPage = 40;
+
+loadMoreBtn.addEventListener('click', async () => {
+  try {
+    const cards = await fetchPopularGames(page);
+    renderGames(cards.results); // Renderowanie tylko danych gier, a nie całego obiektu odpowiedzi
+    page++;
+  } catch (error) {
+    console.error('Error fetching and rendering games:', error);
+  }
+});
