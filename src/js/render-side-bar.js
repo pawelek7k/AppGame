b65b414ab0e2267e3e75399dbeb712d408008bb9
pawelek <k.pawelek@vp.pl>
2024-03-31 15:48:19 +0200

    add nav-bar

--- src/js/render-side-bar.js
+++ src/js/render-side-bar.js
@@ -3,15 +3,13 @@ import { fetchGenres } from './render-cards';
 export async function generateGenresOptions() {
   const genres = await fetchGenres();
 
-  const selectHtml = document.querySelector('#genre-select');
-
-  selectHtml.innerHTML = '';
+  const selectHtml = document.querySelector('#genre-list');
 
   genres.forEach(genre => {
-    const option = document.createElement('option');
-    option.value = genre.id;
-    option.textContent = genre.name;
-    selectHtml.appendChild(option);
+    const listItem = document.createElement('li');
+    listItem.dataset.value = genre.id;
+    listItem.textContent = genre.name;
+    selectHtml.appendChild(listItem);
   });
 }
 
