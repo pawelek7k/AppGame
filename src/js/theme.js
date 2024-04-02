f68e21bdea49e134835d8528648d0e1e563d2728
pawelek <k.pawelek@vp.pl>
2024-04-02 21:35:51 +0200

    add light theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,6 +1,17 @@
 const checkbox = document.querySelector('.switch input[type="checkbox"]');
 const body = document.querySelector('body');
 
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'light') {
+  body.classList.add('light-theme');
+  checkbox.checked = true;
+}
+
 checkbox.addEventListener('change', () => {
   body.classList.toggle('light-theme');
+  if (checkbox.checked) {
+    localStorage.setItem('theme', 'light');
+  } else {
+    localStorage.removeItem('theme');
+  }
 });
