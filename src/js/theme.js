7b5217f4522629a0a5a0a9922fd97cc8982cc190
pawelek <k.pawelek@vp.pl>
2024-04-02 20:32:46 +0200

    attempt light-theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,1 +1,6 @@
 const checkbox = document.querySelector('.switch input[type="checkbox"]');
+const body = document.querySelector('body');
+
+checkbox.addEventListener('change', () => {
+  body.classList.toggle('light-theme');
+});
