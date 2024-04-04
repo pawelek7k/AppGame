50ba0621c2703cebbcc8ced731d76b60a6729c49
pawelek <k.pawelek@vp.pl>
2024-04-04 21:31:39 +0200

    add fixes hamburger

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,12 +1,9 @@
-const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.hamburger-checbox');
 const hamburgerContainer = document.querySelector('.btns-and-theme-hamb');
-
-hamburger.addEventListener('click', () => {
-  hamburgerContainer.classList.add('show-menu');
+hamburger.addEventListener('change', () => {
+  if (hamburger.checked) {
+    hamburgerContainer.classList.add('show-menu');
+  } else {
+    hamburgerContainer.classList.remove('show-menu');
+  }
 });
-
-// hamburger.addEventListener('change', () => {
-//   if (!hamburger.checked) {
-//     hamburgerContainer.classList.remove('show-menu');
-//   }
-// });
