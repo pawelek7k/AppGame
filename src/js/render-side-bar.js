ef2cbc208f85604c2d321761ab2052f5058fbd35
pawelek <k.pawelek@vp.pl>
2024-04-04 20:03:50 +0200

    add genres side-bar

--- src/js/render-side-bar.js
+++ src/js/render-side-bar.js
@@ -1,8 +1,22 @@
-import { fetchGenres } from './render-cards';
+import { fetchGenres, renderGames } from './render-cards';
+
+export async function fetchGamesByGenre(genreId) {
+  const API_KEY = '4688c8e2a1b74736a6eaf922561496b4';
+  const url = new URL(`https://api.rawg.io/api/games`);
+  url.searchParams.append('key', API_KEY);
+  url.searchParams.append('genres', genreId);
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.results;
+  } catch (error) {
+    console.error('Error fetching games for genre:', error);
+  }
+}
 
 export async function generateGenresOptions() {
   const genres = await fetchGenres();
-
   const selectHtml = document.querySelector('#genre-list');
 
   genres.forEach(genre => {
@@ -12,6 +26,12 @@ export async function generateGenresOptions() {
     spanItem.textContent = genre.name;
     selectHtml.appendChild(listItem);
     listItem.appendChild(spanItem);
+
+    listItem.addEventListener('click', async () => {
+      const genreId = listItem.dataset.value;
+      const games = await fetchGamesByGenre(genreId);
+      renderGames(games);
+    });
   });
 }
 
