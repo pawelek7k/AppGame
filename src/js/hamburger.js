113611e62f965dcbd2ad20ed2699aa2803144fcf
pawelek <k.pawelek@vp.pl>
2024-04-05 19:45:33 +0200

    add fixes

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,9 +1,5 @@
 const hamburger = document.querySelector('.hamburger-checbox');
 const hamburgerContainer = document.querySelector('.btns-and-theme-hamb');
 hamburger.addEventListener('change', () => {
-  if (hamburger.checked) {
-    hamburgerContainer.classList.add('show-menu');
-  } else {
-    hamburgerContainer.classList.remove('show-menu');
-  }
+  hamburgerContainer.classList.toggle('show-menu');
 });
