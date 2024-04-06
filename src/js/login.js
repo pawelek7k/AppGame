aab494a903762f4a4a1b66c33b9ef51875763496
pawelek <k.pawelek@vp.pl>
2024-04-06 21:37:33 +0200

    add js

--- src/js/login.js
+++ src/js/login.js
@@ -1,10 +1,15 @@
 const loginContainer = document.querySelector('.login-container');
-
 const loginBtn = document.querySelector(
-  '.buttons-register  button:nth-of-type(1)'
+  '.buttons-register button:nth-of-type(1)'
 );
+const closeBtn = document.querySelector('.close-btn');
 
 loginBtn.addEventListener('click', e => {
   e.preventDefault();
   loginContainer.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', e => {
+  e.preventDefault();
+  loginContainer.style.display = 'none';
 });
