f8468cdd051a182136f74243009ec260d6cfaf45
pawelek <k.pawelek@vp.pl>
2024-04-14 13:17:55 +0200

    add background image user

--- src/js/userIcon.js
+++ src/js/userIcon.js
@@ -1,13 +1,24 @@
 const profilePicInput = document.getElementById('profilePicInput');
 const userIcon = document.getElementById('userIcon');
+const backgroundImg = document.querySelector('.background-image');
 let isInputClicked = false;
 
-userIcon.addEventListener('click', () => {
+const openProfilePicInput = () => {
   if (!isInputClicked) {
     profilePicInput.click();
     isInputClicked = true;
   }
-});
+};
+
+const openBackgroundImgInput = () => {
+  if (!isInputClicked) {
+    backgroundImg.click();
+    isInputClicked = true;
+  }
+};
+
+userIcon.addEventListener('click', openProfilePicInput);
+backgroundImg.addEventListener('click', openBackgroundImgInput);
 
 profilePicInput.addEventListener('change', e => {
   const file = e.target.files[0];
@@ -17,6 +28,18 @@ profilePicInput.addEventListener('change', e => {
 
     userIcon.src = imgUrl;
     userIcon.style.borderRadius = '50%';
+  }
+
+  isInputClicked = false;
+});
+
+backgroundImg.addEventListener('change', e => {
+  const file = e.target.files[0];
+
+  if (file) {
+    const imgUrl = URL.createObjectURL(file);
+
+    backgroundImg.style.backgroundImage = `url(${imgUrl})`;
   }
 
   isInputClicked = false;
