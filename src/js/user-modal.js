5a35314e4e3340b07eff43c0b1b62721b6480fe6
pawelek <k.pawelek@vp.pl>
2024-04-14 20:29:30 +0200

    add fixes

--- src/js/user-modal.js
+++ src/js/user-modal.js
@@ -9,11 +9,42 @@ const userProfileLogOut = `
             <p class="user-location">Location: New York, USA</p>
             <p class="user-interests">Interests: Traveling, Photography, Reading</p>`;
 
-const userProgileLogIn = ``;
+const userProgileLogIn = ` <div class="background-image">
+<label for="backgroundImgInput">
+    <div class="bg-image"></div>
+    <!-- <img src='./images/bg.jpg' alt="Background Image" id="backgroundImg" /> -->
+</label>
+<input type="file" id="backgroundImgInput" style="display: none;">
+</div>
+
+<div class="container-user-icon">
+<label for="profilePicInput">
+    <img src="./images/ProfileUser.png" alt="User Icon" id="userIcon" />
+</label>
+<input type="file" id="profilePicInput" style="display: none;">
+</div>
+
+<p class="nickname">AppGamesNickname</p>
+<ul>
+<li><button>My WishList</button></li>
+<li><button>My Games</button></li>
+</ul>
+<ul class="user-informations">
+<li>
+    <p>Join date:</p>
+</li>
+<li>
+    <p>
+        <span id="copyButton">Copy your ID</span>
+    </p>
+
+</li>
+</ul>
+</div>`;
 
 userIcon.addEventListener('click', e => {
   userModal.style.display = 'block';
-  // userProfileHtml.innerHTML = userProfileLogOut;
+  // userProfileHtml.innerHTML = userProgileLogIn;
 });
 
 const closeButton = userModal.querySelector('.user-modal-close');
